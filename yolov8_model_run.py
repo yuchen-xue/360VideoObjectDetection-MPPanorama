ebf67f2b2b68f4f9fe9153d4fa8ebb02d7ade2f5
Yuchen_XUE <3142-yxue@users.noreply.gitlab.univ-lr.fr>
2026-01-20 10:36:55 +0100

    Use Ultralytics for model inference

--- yolov8_model_run.py
+++ yolov8_model_run.py
@@ -1,20 +1,11 @@
-# Ultralytics YOLO 🚀, AGPL-3.0 license
-
-import argparse
-
-import cv2.dnn
-import numpy as np
-
-from ultralytics.utils import YAML
-from ultralytics.utils.checks import check_yaml
-from draw_boxes import draw_bounding_box
-from PIL import Image
-
-CLASSES = YAML.load(check_yaml("coco128.yaml"))["names"]
-colors = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+from ultralytics import YOLO
 
 
-def detect(input_array, confidence_threshold=0.40, onnx_model="model-runs/detect/train/weights/yolov8n.onnx"):
+def detect(
+    input_array,
+    confidence_threshold=0.40,
+    onnx_model="model-runs/detect/train/weights/yolov8n.onnx",
+):
     """
     Function:
     Load ONNX model, perform inference, draw bounding boxes, and display the output image.
@@ -27,102 +18,45 @@ def detect(input_array, confidence_threshold=0.40, onnx_model="model-runs/detect
         detections: Array of detections (list) with info such as class_id, class_name, confidence, etc.
     """
     # Load the ONNX model
-    model: cv2.dnn.Net = cv2.dnn.readNetFromONNX(onnx_model)
-
-    # Read the input image
-    original_image = input_array
-    [height, width, _] = original_image.shape
-
-    # Prepare a square image for inference
-    length = max((height, width))
-    image = np.zeros((length, length, 3), np.uint8)
-    image[0:height, 0:width] = original_image
-
-    # Calculate scale factor
-    scale = length / 640
-
-    # Preprocess the image and prepare blob for model
-    blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=True)
-    model.setInput(blob)
+    model = YOLO(onnx_model)
 
     # Perform inference
-    outputs = model.forward()
+    results = model.predict(source=input_array, conf=confidence_threshold, save=False, verbose=False)
 
-    # Prepare output array
-    outputs = np.array([cv2.transpose(outputs[0])])
-    rows = outputs.shape[1]
+    detections: list[dict] = []
 
-    boxes = []
-    scores = []
-    class_ids = []
+    for result in results:
+        # Calculate scale factor of the input image
+        length = max(result.orig_shape)
+        scale = length / 640
 
-    # Iterate through output to collect bounding boxes, confidence scores, and class IDs
-    for i in range(rows):
-        classes_scores = outputs[0][i][4:]
-        (_, maxScore, _, (_, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
-        if maxScore >= confidence_threshold:
-            box = [
-                outputs[0][i][0] - (0.5 * outputs[0][i][2]),
-                outputs[0][i][1] - (0.5 * outputs[0][i][3]),
-                outputs[0][i][2],
-                outputs[0][i][3],
-            ]
-            boxes.append(box)
-            scores.append(maxScore)
-            class_ids.append(maxClassIndex)
+        # Iterate through detection results
+        for j in range(len(result.boxes)):
+            box = result.boxes[j]
 
-    # Apply NMS (Non-maximum suppression)
-    result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
+            # Get x, y coordinates of the top-left corner
+            box_xyxy = box.xyxy[0]
+            topleft_x = int(box_xyxy[0].item())
+            topleft_y = int(box_xyxy[1].item())
 
-    detections = []
+            # Get box width and height
+            box_xywh = box.xywh[0]
+            width = int(box_xywh[2].item())
+            height = int(box_xywh[3].item())
 
-    # Iterate through NMS results to draw bounding boxes and labels
-    for i in range(len(result_boxes)):
-        index = result_boxes[i]
-        box = boxes[index]
-        detection = {
-            "class_id": class_ids[index],
-            "class_name": CLASSES[class_ids[index]],
-            "confidence": scores[index],
-            "box": box,
-            "scale": scale,
-        }
-        detections.append(detection)
-        draw_bounding_box(
-            original_image,
-            class_ids[index],
-            scores[index],
-            round(box[0] * scale),
-            round(box[1] * scale),
-            round((box[0] + box[2]) * scale),
-            round((box[1] + box[3]) * scale),
-        )
+            # Get class id, class name, and confidence score
+            class_id = int(box.cls.item())
+            class_name = model.names[class_id]
+            confidence = box.conf.item()
 
-    # Display the image with bounding boxes
-    '''
-    For testing purposes to show the bounding boxes on the stereographic projected image
-    '''
-    # cv2.imshow("image with detections", original_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+            detections.append(
+                {
+                    "class_id": class_id,
+                    "class_name": class_name,
+                    "confidence": confidence,
+                    "box": [topleft_x, topleft_y, width, height],
+                    "scale": scale,
+                }
+            )
 
     return detections
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", default="model-runs/detect/train/weights/yolov8n.onnx", help="Input your ONNX model.")
-    parser.add_argument("--img", help="Path to input image.")
-    parser.add_argument("--output", help="Path to output image.", default=None)
-    args = parser.parse_args()
-
-    original_image: np.ndarray = cv2.imread(args.img)
-    print(original_image.shape)
-    detect(original_image, args.model)
-    if args.output:
-        image_height = original_image.shape[0]
-        image_width = original_image.shape[1]        
-
-        output_image = Image.fromarray(original_image.reshape((image_height, image_width, 3)).astype('uint8'), 'RGB')
-        
-        cv2.imwrite(args.output)
